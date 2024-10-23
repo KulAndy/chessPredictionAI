79c2bbf2e674cd4f8599ca89d2701d16bd7c84eb
Andrzej Kulesza <andykrk22@gmail.com>
2024-10-23 09:15:30 +0200

    strategy mirror

--- learning.py
+++ learning.py
@@ -10,73 +10,84 @@ client = MongoClient(connection_string)
 db = client[SETTINGS['mongo']['database']]
 collection = db[SETTINGS['mongo']['collection']]
 
+strategy = tf.distribute.MirroredStrategy()
 
-def learn(batch_size=200_000):
+
+def learn(batch_size=300_000):
     docs_count = collection.count_documents({})
     batch_start = 0
 
-    model = None
-    input_shape = (None, 3)
+    with strategy.scope():
+        model = None
+        input_shape = (None, 3)
 
-    while batch_start < docs_count:
-        print(f"Processing batch {batch_start} to {batch_start + batch_size}")
-        docs = collection.find({}).skip(batch_start).limit(batch_size)
-        x_train = []
-        y_train = []
+        while batch_start < docs_count:
+            print(f"Processing batch {batch_start} to {batch_start + batch_size}")
+            docs = collection.find({}).skip(batch_start).limit(batch_size)
+            x_train = []
+            y_train = []
 
-        for doc in docs:
-            input_data = np.array(doc['input'])  # array of 3D vectors, coords between 0 and 1
-            output_data = np.array(doc['output'])  # float between 0 and 1
-            x_train.append(input_data)
-            y_train.append(output_data)
+            for doc in docs:
+                input_data = np.array(doc['input'])  # array of 3D vectors, coords between 0 and 1
+                output_data = np.array(doc['output'])  # float between 0 and 1
 
-        x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, padding='post', dtype='float32', value=0.0)
-        y_train = np.array(y_train)
+                output_data = np.log(output_data + 1e-8)
 
-        dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-        dataset = dataset.shuffle(buffer_size=len(x_train)).batch(2).prefetch(tf.data.AUTOTUNE)
+                x_train.append(input_data)
+                y_train.append(output_data)
 
-        if model is None:
-            model = models.Sequential()
-            model.add(layers.Input(shape=input_shape))
-            model.add(layers.TimeDistributed(layers.Dense(32, activation='relu')))
-            model.add(layers.GlobalAveragePooling1D())
-            model.add(layers.Dense(1, activation='sigmoid'))
+            x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, padding='post', dtype='float32', value=0.0)
+            y_train = np.array(y_train)
 
-            model.compile(optimizer='adam', loss='mse')
-            model.summary()
+            dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+            dataset = dataset.shuffle(buffer_size=len(x_train)).batch(256).prefetch(tf.data.AUTOTUNE)
 
-        model.fit(dataset, epochs=10)
+            if model is None:
+                model = models.Sequential()
+                model.add(layers.Input(shape=input_shape))
+                model.add(layers.TimeDistributed(layers.Dense(32, activation='relu')))
+                model.add(layers.GlobalAveragePooling1D())
 
-        if batch_start + batch_size < docs_count:
-            print(f"Testing on next batch {batch_start + batch_size} to {batch_start + 2 * batch_size}")
-            test_docs = collection.find({}).skip(batch_start + batch_size).limit(batch_size)
-            x_test = []
-            y_test = []
+                model.add(layers.Dense(1, activation='sigmoid'))
 
-            for doc in test_docs:
-                input_data = np.array(doc['input'])
-                output_data = np.array(doc['output'])
-                x_test.append(input_data)
-                y_test.append(output_data)
+                model.compile(optimizer='adam', loss='mse')
+                model.summary()
 
-            x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, padding='post', dtype='float32', value=0.0)
-            y_test = np.array(y_test)
+            model.fit(dataset, epochs=10)
 
-            test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-            test_dataset = test_dataset.batch(2).prefetch(tf.data.AUTOTUNE)
+            if batch_start + batch_size < docs_count:
+                print(f"Testing on next batch {batch_start + batch_size} to {batch_start + 2 * batch_size}")
+                test_docs = collection.find({}).skip(batch_start + batch_size).limit(batch_size)
+                x_test = []
+                y_test = []
 
-            loss = model.evaluate(test_dataset)
-            print(f"Test loss: {loss}")
+                for doc in test_docs:
+                    input_data = np.array(doc['input'])
+                    output_data = np.array(doc['output'])
 
-            model.fit(test_dataset, epochs=1)
+                    output_data = np.log(output_data + 1e-8)
 
-        batch_start += batch_size
+                    x_test.append(input_data)
+                    y_test.append(output_data)
 
-    if not os.path.exists(SETTINGS['model_dir']):
-        os.mkdir(SETTINGS['model_dir'])
+                x_test = tf.keras.preprocessing.sequence.pad_sequences(x_test, padding='post', dtype='float32',
+                                                                       value=0.0)
+                y_test = np.array(y_test)
 
-    model.save(SETTINGS['model_dir'] + '/model.keras')
+                test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+                test_dataset = test_dataset.batch(256).prefetch(tf.data.AUTOTUNE)
+
+                loss = model.evaluate(test_dataset)
+                print(f"Test loss: {loss}")
+
+                model.fit(test_dataset, epochs=1)
+
+            batch_start += batch_size
+
+        if not os.path.exists(SETTINGS['model_dir']):
+            os.mkdir(SETTINGS['model_dir'])
+
+        model.save(SETTINGS['model_dir'] + '/model.keras')
 
 
 def load_and_predict(new_data):
